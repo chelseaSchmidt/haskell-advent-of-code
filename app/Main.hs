ef29b238211dd1706c4c48a2c65b8fe5e344316f
Chelsea Schmidt <cschmidt331@gmail.com>
2024-12-01 15:38:35 -0800

    Day 1 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Y2019.Day01 (printY2019Day01Part1, printY2019Day01Part2)
 import Y2019.Day02 (printY2019Day02Part1)
-import Y2024.Day01 (printY2024Day01Part1)
+import Y2024.Day01 (printY2024Day01Part1, printY2024Day01Part2)
 
 main :: IO ()
 main = do
@@ -10,3 +10,4 @@ main = do
   printY2019Day01Part2
   printY2019Day02Part1
   printY2024Day01Part1
+  printY2024Day01Part2
--- src/Y2024/Day01.hs
+++ src/Y2024/Day01.hs
@@ -6,6 +6,9 @@ import Data.Bifunctor (Bifunctor(bimap))
 
 data TypedTuple a = a :&: a
 type Location = Int
+type SimilarityScore = Int
+type SimilarityScores = [Int]
+type ScoredLocation = (Location, SimilarityScore)
 
 printY2024Day01Part1 :: IO ()
 printY2024Day01Part1 = readInputFileByName "2024-01" >>= print
@@ -17,6 +20,18 @@ printY2024Day01Part1 = readInputFileByName "2024-01" >>= print
   where
     zipLists (a, b) = zipWith (:&:) a b
     toDistance (a :&: b) = abs (a - b)
+
+printY2024Day01Part2 :: IO ()
+printY2024Day01Part2 = readInputFileByName "2024-01" >>= print
+  . sum
+  . toSimilarityScores
+  . parse
+  where
+    toSimilarityScores :: ([Location], [Location]) -> SimilarityScores
+    toSimilarityScores (leftList, rightList) = map (scoreSimilarity rightList) leftList
+      where
+        scoreSimilarity :: [Location] -> Location -> SimilarityScore
+        scoreSimilarity rightLocations leftLoc = leftLoc * length (filter (== leftLoc) rightLocations)
 
 parse :: String -> ([Location], [Location])
 parse = bimap (map fst) (map fst)
