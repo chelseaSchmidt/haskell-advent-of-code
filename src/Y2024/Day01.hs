b5f7c9f383505bbafe51b66c549e963742bc2022
Chelsea Schmidt <cschmidt331@gmail.com>
2024-12-01 16:07:21 -0800

    Implement fmap for custom & type

--- src/Y2024/Day01.hs
+++ src/Y2024/Day01.hs
@@ -2,23 +2,26 @@ module Y2024.Day01 where
 import ReadInputFile (readInputFileByName)
 import Control.Monad (join)
 import Data.List (partition, sort)
-import Data.Bifunctor (Bifunctor(bimap))
 
-data TypedTuple a = a :&: a
+data (:&:) a = a :&: a
 type Location = Int
 type SimilarityScore = Int
 type SimilarityScores = [Int]
 type ScoredLocation = (Location, SimilarityScore)
+
+instance Functor (:&:) where
+  fmap :: (a -> b) -> (:&:) a -> (:&:) b
+  fmap f (a :&: b) = f a :&: f b
 
 printY2024Day01Part1 :: IO ()
 printY2024Day01Part1 = readInputFileByName "2024-01" >>= print
   . sum
   . map toDistance
   . zipLists
-  . bimap sort sort
+  . fmap sort
   . parse
   where
-    zipLists (a, b) = zipWith (:&:) a b
+    zipLists (a :&: b) = zipWith (:&:) a b
     toDistance (a :&: b) = abs (a - b)
 
 printY2024Day01Part2 :: IO ()
@@ -27,14 +30,18 @@ printY2024Day01Part2 = readInputFileByName "2024-01" >>= print
   . toSimilarityScores
   . parse
   where
-    toSimilarityScores :: ([Location], [Location]) -> SimilarityScores
-    toSimilarityScores (leftList, rightList) = map (scoreSimilarity rightList) leftList
+    toSimilarityScores :: (:&:) [Location] -> SimilarityScores
+    toSimilarityScores (leftList :&: rightList) = map (scoreSimilarity rightList) leftList
       where
         scoreSimilarity :: [Location] -> Location -> SimilarityScore
         scoreSimilarity rightLocations leftLoc = leftLoc * length (filter (== leftLoc) rightLocations)
 
-parse :: String -> ([Location], [Location])
-parse = bimap (map fst) (map fst)
+tupleToTypedPair :: (a, a) -> (:&:) a
+tupleToTypedPair (a, b) = a :&: b
+
+parse :: String -> (:&:) [Location]
+parse = fmap (map fst)
+  . tupleToTypedPair
   . partition snd
   . join
   . map (toLocationMetadataList . toWordPair . words)
