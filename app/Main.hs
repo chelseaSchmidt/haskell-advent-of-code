87502bd903dab54c8dfc9cf6133e71f250558d70
Chelsea Schmidt <cschmidt331@gmail.com>
2024-12-03 15:53:53 -0800

    Day 3 part 1

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Y2019.Day01 (printY2019Day01Part1, printY2019Day01Part2)
 import Y2019.Day02 (printY2019Day02Part1)
 import Y2024.Day01 (printY2024Day01Part1, printY2024Day01Part2)
 import Y2024.Day02 (printY2024Day02Part1, printY2024Day02Part2)
+import Y2024.Day03 (printY2024Day03Part1)
 
 main :: IO ()
 main = do
@@ -14,3 +15,4 @@ main = do
   printY2024Day01Part2 -- 23082277
   printY2024Day02Part1 -- 624
   printY2024Day02Part2 -- 658
+  printY2024Day03Part1 -- 165225049
---
+++ src/Y2024/Day03.hs
@@ -0,0 +1,55 @@
+module Y2024.Day03 where
+import ReadInputFile (readInputFileByName)
+
+type Instruction = (String, String)
+type CorruptInstructions = String
+type PartialInt = String
+type UnparsedInt = String
+type PartialInstructions = [Instruction]
+type ValidInstructions = [Instruction]
+
+printY2024Day03Part1 :: IO ()
+printY2024Day03Part1 = readInputFileByName "2024-03" >>= print . sum . map toProduct . parse
+
+-- valid sequence: "m", "u", "l", "(", "\d+", "," "\d+", ")"
+parse :: CorruptInstructions -> ValidInstructions
+parse = go []
+  where
+    go :: PartialInstructions -> CorruptInstructions -> ValidInstructions
+    go partialInst ('m':'u':'l':'(':cs) =
+      go
+        (if areIntsValid then partialInst ++ [(firstInt, secondInt)] else partialInst)
+        (if areIntsValid then drop 1 theRestOfTheRest else cs)
+      where
+        firstInt = fst (parseInt cs)
+        theRest = snd (parseInt cs)
+        secondInt = fst (parseInt (drop 1 theRest))
+        theRestOfTheRest = snd (parseInt (drop 1 theRest))
+        maybeComma = if not (null theRest) then head theRest else 'x'
+        maybeParen = if not (null theRestOfTheRest) then head theRestOfTheRest else 'x'
+        isFirstIntValid =  not (null firstInt)
+        isSecondIntValid =  not (null secondInt)
+        areIntsValid = maybeComma == ',' && maybeParen == ')' && isFirstIntValid && isSecondIntValid
+    go partialInst (_:cs) = go partialInst cs
+    go partialInst _ = partialInst
+
+parseInt :: CorruptInstructions -> (UnparsedInt, CorruptInstructions)
+parseInt = go ""
+  where
+    go :: PartialInt -> CorruptInstructions -> (UnparsedInt, CorruptInstructions)
+    go partialInt [] = (partialInt, [])
+    go partialInt (c:cs) = case c of
+      '0' -> go (partialInt ++ [c]) cs
+      '1' -> go (partialInt ++ [c]) cs
+      '2' -> go (partialInt ++ [c]) cs
+      '3' -> go (partialInt ++ [c]) cs
+      '4' -> go (partialInt ++ [c]) cs
+      '5' -> go (partialInt ++ [c]) cs
+      '6' -> go (partialInt ++ [c]) cs
+      '7' -> go (partialInt ++ [c]) cs
+      '8' -> go (partialInt ++ [c]) cs
+      '9' -> go (partialInt ++ [c]) cs
+      _ -> (partialInt, c:cs)
+
+toProduct :: Instruction -> Int
+toProduct i = read (fst i) * read (snd i)
