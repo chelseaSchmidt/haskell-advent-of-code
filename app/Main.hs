7b63c56aaee838f0eb6a7eca188f972bcc1feb19
Chelsea Schmidt <cschmidt331@gmail.com>
2024-12-02 21:04:35 -0800

    Day 2 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Y2019.Day01 (printY2019Day01Part1, printY2019Day01Part2)
 import Y2019.Day02 (printY2019Day02Part1)
 import Y2024.Day01 (printY2024Day01Part1, printY2024Day01Part2)
-import Y2024.Day02 (printY2024Day02Part1)
+import Y2024.Day02 (printY2024Day02Part1, printY2024Day02Part2)
 
 main :: IO ()
 main = do
@@ -13,3 +13,4 @@ main = do
   printY2024Day01Part1 -- 2375403
   printY2024Day01Part2 -- 23082277
   printY2024Day02Part1 -- 624
+  printY2024Day02Part2
--- src/Y2024/Day02.hs
+++ src/Y2024/Day02.hs
@@ -4,27 +4,35 @@ import ReadInputFile (readInputFileByName)
 type Input = String
 type Level = Int
 type Report = [Level]
+type PreviousReport = [Level]
 type Diffs = [Int]
+type Freebies = Int
 data Direction = Asc | Desc deriving (Eq)
 
 printY2024Day02Part1 :: IO ()
-printY2024Day02Part1 = readInputFileByName "2024-02" >>= print . length . filter id . map isReportSafe . parse
+printY2024Day02Part1 = readInputFileByName "2024-02" >>= print . length . filter id . map (isReportSafe 0) . parse
+
+printY2024Day02Part2 :: IO ()
+printY2024Day02Part2 = readInputFileByName "2024-02" >>= print . length . filter id . map (isReportSafe 1) . parse
+
+isReportSafe :: Freebies -> Report -> Bool
+isReportSafe f r = isSortedIshAndInRange f Asc [] r || isSortedIshAndInRange f Desc [] r
   where
-    isReportSafe :: Report -> Bool
-    isReportSafe r = (isSortedIsh Asc r || isSortedIsh Desc r) && areDiffsInRange (reportToDiffs r)
+    -- FIXME: refactor to use an inner recursive function
+    isSortedIshAndInRange :: Freebies -> Direction -> PreviousReport -> Report -> Bool
+    isSortedIshAndInRange _ _ _ [] = True
+    isSortedIshAndInRange _ _ _ [_] = True
+    isSortedIshAndInRange freebies dir prev (x:y:zs)
+      | isPassing = isSortedIshAndInRange freebies dir (prev ++ [x]) (y:zs)
+      | freebies > 0 =
+        isSortedIshAndInRange (freebies - 1) dir [] (prev ++ y:zs)
+        || isSortedIshAndInRange (freebies - 1) dir [] (prev ++ x:zs)
+      | otherwise = False
       where
-        isSortedIsh :: Direction -> Report -> Bool
-        isSortedIsh _ [] = True
-        isSortedIsh _ [_] = True
-        isSortedIsh d (x:y:zs) = (if d == Asc then (<) else (>)) x y && isSortedIsh d (y:zs)
-
-        areDiffsInRange :: Diffs -> Bool
-        areDiffsInRange d = not (any (< 1) d) && not (any (> 3) d)
-
-        reportToDiffs :: Report -> Diffs
-        reportToDiffs [] = []
-        reportToDiffs [_] = [1]
-        reportToDiffs (a:b:cs) = abs (a - b) : reportToDiffs (b:cs)
+        isPassing =
+          (if dir == Asc then (<) else (>)) x y
+          && abs (x - y) >= 1
+          && abs (x - y) <= 3
 
 parse :: Input -> [Report]
 parse = map (map read . words) . lines
